9d9cc76b1e0052d93e677f5630c33fada5bb82c0
emilyboo <ehbookstein@gmail.com>
2014-10-17 17:04:38 -0700

    Fixed error: changed request.args.get(student) to get(github) since user will enter a github name and receive full name.

--- extra_practice_SQL/webapp.py
+++ extra_practice_SQL/webapp.py
@@ -4,14 +4,22 @@ import hackbright_app
 app = Flask(__name__)
 
 @app.route("/")
+def show_home():
+	date = "10/17/14"
+	html = render_template("home.html", date = date)
+	return html
+
+@app.route("/student")
 def get_student():
 	hackbright_app.connect_to_DB()
 	#request.args is a dictionary
 	#.get() retrieves key's value if it is in dict
 	#key is "student"
-	student_github = request.args.get("student")
-	return hackbright_app.get_student_by_github(student_github)
-
+	student_github = request.args.get("github")
+	row = hackbright_app.get_student_by_github(student_github)
+	html = render_template("student_info.html", first_name = row[0],
+		last_name = row[1], github = row[2])
+	return html
 
 if __name__ == "__main__":
 	app.run(debug=True)
